c0a0be55ff477408b932c9a5ffbbf65e960703b3
cuixiaohui233 <3176054945@qq.com>
2017-08-05 22:59:35 +0800

    8-5biji

--- 8-4node/my-jsonp-1.0.js
+++ 8-4node/my-jsonp-1.0.js
@@ -3,17 +3,38 @@ function jsonp(obj){
 		dataType:'',
 		url:'',
 		data:{},
+		cb:'cb',
 		success:function(){}
 	}
 	
 	Object.assign(setting,obj);
 	
+	
+	
+	let fnName = 'jQuery' + Math.random() + (+new Date); 
+	fnName = fnName.replace('0.','1_');
+	
+	window[fnName] = function(data){
+		setting.success(data);
+	}
+	if(setting.cb){
+		setting.data[setting.cb] = fnName;
+	}
+	
+//	console.log(setting.data)
 	let arr = [];
 	
 	for(var attr in setting.data){
 		arr.push(attr+'='+setting.data[attr]);
 	}
-	console.log(arr);
+//	console.log(arr);
 	var str = arr.join('&');
 //	console.log(str);
+	
+	let script = document.createElement('script');
+	
+	script.src = setting.url+ '?' + str;
+	console.log(script.src);
+	document.getElementsByTagName('head')[0].appendChild(script);
+	script.remove();
 }
